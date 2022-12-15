778bb3bba3d4931ff58ffaf7b448a70bfb045d3d
Frederico Velho <fredericovcoelho@gmail.com>
2022-12-15 01:06:37 -0300

    feat(use-cases): implementing send-notification use case.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './infra/app.controller';
+import { PrismaService } from './infra/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
 })
 export class AppModule {}
